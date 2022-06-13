976ea98b99e35dad70c4ae75e42457af0aea4c97
CarlosTajamar <84123416+CarlosTajamar@users.noreply.github.com>
2022-06-13 14:40:27 +0200

    Add files via upload

---
+++ JavaScript/scripts/forgot1.js
@@ -0,0 +1,16 @@
+function password(form) {
+	username = new Array("carlos", "emilio", "anonimo");
+
+	if(form.userid.value == username[0]) {
+		alert("Las contraseñas de administrador no se pueden restablecer. Póngase en contacto con su administrador para recuperar su contraseña")/* Admin */
+	}
+	else if(form.userid.value == username[1]) {
+		alert("Las contraseñas de administrador no se pueden restablecer. Póngase en contacto con su administrador para recuperar su contraseña")/* Admin */
+	}
+	else if(form.userid.value == username[2]) {
+		location.replace('find-account.html')/* Members reset password */
+	}
+	else {
+		alert("No se encontró tal usuario")
+	}
+}
